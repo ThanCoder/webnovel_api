2cdf9332a6113591374c52b2e507ebe1ebea6daf
thancoder <thancoder2019@gmail.com>
2025-06-08 16:52:51 +0630

    changed

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { html } from 'hono/html';
+const app = new Hono();
+app.get('/', (c) => {
+    return c.html(html `<h1>hello</h1>`);
+});
+serve({
+    fetch: app.fetch,
+    port: 3000
+}, (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+});
